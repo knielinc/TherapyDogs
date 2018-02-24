794aeb090f560affdfa7feb7b5c105760377b849
jurech <jurech@student.ethz.ch>
2018-02-24 20:31:32 +0100

    Merge branch 'master' of C:\Users\Jan\IdeaProjects\TheRapyDogs with conflicts.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,14 @@
 import com.pi4j.io.gpio.*;
 
+
 public class Main {
     //GPIO Pins
-    private static GpioPinDigitalOutput sensorTriggerPin;
-    private static GpioPinDigitalInput sensorEchoPin;
 
-    final static GpioController gpio = GpioFactory.getInstance();
 
     public static void main(String[] args) {
-        sensorTriggerPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00); // Trigger pin as OUTPUT
-        sensorEchoPin = gpio.provisionDigitalInputPin(RaspiPin.GPIO_01, PinPullResistance.PULL_DOWN); // Echo pin as INPUT
+        UltraSens ultraSens = new UltraSens();
+        Controller cont = new Controller(ultraSens);
 
-        while (true) {
-            try {
-                Thread.sleep(100);
-                sensorTriggerPin.high(); // Make trigger pin HIGH
-
-                Thread.sleep((long) 0.01);// Delay for 10 microseconds
-                sensorTriggerPin.low(); //Make trigger pin LOW
-
-                while (sensorEchoPin.isLow()) { //Wait until the ECHO pin gets HIGH
-
-                }
-                long startTime = System.nanoTime(); // Store the surrent time to calculate ECHO pin HIGH time.
-                while (sensorEchoPin.isHigh()) { //Wait until the ECHO pin gets LOW
-
-                }
-                long endTime = System.nanoTime(); // Store the echo pin HIGH end time to calculate ECHO pin HIGH time.
-
-                System.out.println("Distance :" + ((((endTime - startTime) / 1e3) / 2) / 29.1) + " cm"); //Printing out the distance in cm
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        cont.startFlight();
     }
 }
