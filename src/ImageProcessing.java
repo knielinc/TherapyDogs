364405af57308dc58e850ee6385d2f61d884bdf9
llingens <llingens@student.ethz.ch>
2018-02-24 05:51:46 +0100

    fancy picture evaluation

---
+++ src/ImageProcessing.java
@@ -0,0 +1,33 @@
+import java.awt.image.BufferedImage;
+
+public class ImageProcessing {
+
+	private static final int threshold = 30;
+
+	public static int getScore(BufferedImage input) {
+		int score = 0;
+		// TODO save width height permanently, compare input and second scales
+		// if necessary
+		int width = input.getWidth();
+		int height = input.getHeight();
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				int clr = input.getRGB(x, y);
+				int red = (clr & 0x00ff0000) >> 16;
+				int green = (clr & 0x0000ff00) >> 8;
+				score -= red;
+				score += green;
+			}
+		}
+		score /= width * height;
+		System.out.println("score: " + score);
+		return score;
+	}
+
+	public static boolean changedColor(BufferedImage first, BufferedImage second) {
+		if (Math.abs(getScore(first) - getScore(second)) < threshold)
+			return false;
+		return true;
+	}
+
+}
