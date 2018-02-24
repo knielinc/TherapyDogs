831e8980d2a4fb80401b4ed80d4518d9eae4c560
David Enderlin <davue@bluewin.ch>
2018-02-24 04:28:03 +0100

    Initial commit

---
+++ src/main/java/Camera.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Camera {
+    ProcessBuilder camera = new ProcessBuilder("raspistill", "-t 0", "-o ");
+
+    public static Image takePicture(String filename) {
+        return null;
+    }
+}
