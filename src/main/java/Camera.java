4f7574acba7eb8095a4106b6818344ef09357603
David Enderlin <davue@bluewin.ch>
2018-02-24 11:09:51 +0100

    Working on faster image capturing

--- src/main/java/Camera.java
+++ src/main/java/Camera.java
@@ -1,9 +1,26 @@
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class Camera {
+    public static void startCamera() {
+        try {
+            ProcessBuilder camera = new ProcessBuilder("raspistill", "-t", "0", "-w", "1640", "-h", "922", "-q", "10", "-s", "-o", "temp.jpg");
+            Process cameraProcess = camera.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Image takePreparedPicture() {
+        ProcessBuilder signal = new ProcessBuilder("kill", "");
+
+        return null;
+    }
+
     /**
      * Takes a picture with the given filename and returns an Image object of the image taken.
      *
@@ -12,7 +29,7 @@ public class Camera {
      */
     public static BufferedImage takePicture(String filename) {
         try {
-            ProcessBuilder camera = new ProcessBuilder("raspistill", "-t", "1", "-o", filename);
+            ProcessBuilder camera = new ProcessBuilder("raspistill", "-t", "2", "-o", filename);
             Process cameraProcess = camera.start();
             cameraProcess.waitFor();
 
