549cedb23ebbf4905b44628b34a352c5a8d22930
jurech <jurech@student.ethz.ch>
2018-02-24 21:11:11 +0100

    Implemented Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,57 @@
 import com.pi4j.io.gpio.*;
 
+import java.awt.image.BufferedImage;
+
 
 public class Main {
-    //GPIO Pins
-
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         UltraSens ultraSens = new UltraSens();
         Controller cont = new Controller(ultraSens);
 
-        cont.startFlight();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            FlightController.setArm(1000);
+            FlightController.setRunning(false);
+        }));
+
+        // Start flight controller
+        new Thread(new FlightController()).start();
+
+        FlightController.setArm(1000);
+        FlightController.setThrust(1000);
+        FlightController.setPitch(1500);
+        FlightController.setRoll(1500);
+        FlightController.setYaw(1500);
+
+        Thread.sleep(2000);
+
+        FlightController.setArm(2000);
+        FlightController.setThrust(1000);
+        FlightController.setPitch(1500);
+        FlightController.setRoll(1500);
+        FlightController.setYaw(1500);
+
+        Thread.sleep(2000);
+
+        Camera.startCamera();
+
+        System.out.println("I am ready!");
+
+        BufferedImage lastImage = Camera.takePreparedPicture();
+
+        while (true) {
+            BufferedImage curImage = Camera.takePreparedPicture();
+
+            if (ImageProcessing.changedColor(lastImage, curImage)) {
+                break;
+            }
+
+            lastImage = curImage;
+        }
+
+        Camera.stopCamera();
+
+        while (true) {
+            cont.startFlight();
+        }
     }
 }
