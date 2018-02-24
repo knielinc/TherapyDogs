a736a846e7c7b593e70029dbb287ad5fcfbe932b
David Enderlin <davue@bluewin.ch>
2018-02-24 04:55:27 +0100

    Added method to take images and fixed java version

--- src/main/java/Camera.java
+++ src/main/java/Camera.java
@@ -1,9 +1,26 @@
-import java.awt.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Camera {
-    ProcessBuilder camera = new ProcessBuilder("raspistill", "-t 0", "-o ");
+    /**
+     * Takes a picture with the given filename and returns an Image object of the image taken.
+     *
+     * @param filename the file name of the new image
+     * @return an Image object of the image taken
+     */
+    public static BufferedImage takePicture(String filename) {
+        try {
+            ProcessBuilder camera = new ProcessBuilder("raspistill", "-t 1", "-o " + filename);
+            Process cameraProcess = camera.start();
+            cameraProcess.waitFor();
 
-    public static Image takePicture(String filename) {
+            File imageFile = new File(filename);
+            return ImageIO.read(imageFile);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
